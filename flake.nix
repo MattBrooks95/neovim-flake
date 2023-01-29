1728cfc83b9546702336ef7c9f05ff56c8d0ab9e
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-01-29 13:09:24 +0900

    got to the part where the nvim build steps are complaining about the missing build dependencies, like libUV

--- flake.nix
+++ flake.nix
@@ -2,12 +2,34 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs";
 		flake-utils.url = "github:numtide/flake-utils";
+		# neovim 0.8.2
+		neovim.url = "git+https://github.com/neovim/neovim?ref=release-0.8&rev=1fa917f9a1585e3b87d41edaf74415505d1bceac";
+		neovim.flake = false;
 	};
-	outputs = { self, nixpkgs, flake-utils }:
-	flake-utils.lib.eachDefaultSystem(system:
-		let pkgs = nixpkgs.legacyPackages.${system};
-		in {
-			foo = "bar";
-		}
+	#flake-utils is an abstraction that saves us from needing to specify all the architectures
+	#that our package supports
+	outputs = { self, nixpkgs, flake-utils, neovim }: flake-utils.lib.eachDefaultSystem(system:
+			let pkgs = nixpkgs.legacyPackages.${system};
+				packageName = "neovim-flake";
+				neovim-flake = (with pkgs; stdenv.mkDerivation {
+					pname = packageName;
+					version = "0.0.1";
+					src = neovim;
+					nativeBuildInputs = [
+						clang
+						cmake
+					];
+					buildPhase = ''cd $TMP/neovim &&\
+						make -j $NIX_BUILD_CORES CMAKE_BUILD_TYPE=Release
+					'';
+					installPhase = ''
+						mkdir -p $out/bin
+						mv $TMP/build/bin/nvim $out/bin
+					'';
+				});
+			in {
+				defaultPackage = neovim-flake;
+				foo = "bar";
+			}
 	);
 }
