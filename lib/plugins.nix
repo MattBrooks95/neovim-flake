f3e4f198625fc3c0e700e2f0f6cab909d8853967
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-03-30 21:11:53 +0900

    tab

--- lib/plugins.nix
+++ lib/plugins.nix
@@ -1,6 +1,6 @@
 rec {
   #needs appended to $out in the build phase
-	packDir = "share/nvim/runtime/pack";
+  packDir = "share/nvim/runtime/pack";
   colorSchemePackageDirName = "colorschemes";
   languageServerPackageDirName = "language-server";
   languagePluginsPackageDirName = "languages";
